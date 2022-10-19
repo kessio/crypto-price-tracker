3ab17ca39813909d08408f1aabcbde6c6d6c810b
unknown <sharonkessio6@gmail.com>
2022-10-19 08:03:39 +0300

    setup create react react, tailwind css, linters, font and color palletes

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const NavBar = () => {
+  return (
+    <h1 className="text-3xl font-bold underline bg-primary font-lato text-light">
+      Hello world!
+    </h1>
+  )
+}
