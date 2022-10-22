71f13a289ae2d9da9e486624fe0406bfb73f0d1f
unknown <sharonkessio6@gmail.com>
2022-10-22 15:08:32 +0300

    chnages

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,6 @@
 import React, { useState } from 'react';
 import { FaBars, FaTimes } from 'react-icons/fa';
 import { BsFillMicFill } from 'react-icons/bs';
-import { Link } from 'react-router-dom';
 
 const NavBar = () => {
   const [nav, setNav] = useState(false);
