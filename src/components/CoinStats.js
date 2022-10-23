e2e79a5cd690d40697002b83ae96b9efbe1e4b31
unknown <sharonkessio6@gmail.com>
2022-10-23 13:53:27 +0300

    Design chnages

--- src/components/CoinStats.js
+++ src/components/CoinStats.js
@@ -1,20 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import CoinsHeadings from './CoinsHeadings';
+import Coins from './Coins';
 
-const CoinStats = () => (
-  <div className="flex justify-between items-center bg-primary h-56">
-    <div className="flex flex-col text-white ml-4 pt-10">
-      <span className="font-lato font-extrabold text-2xl">$923B</span>
-      <span>Market Cap</span>
+const CoinStats = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  return (
+    <div>
+      <div className="flex justify-center items-center bg-light h-56">
+        <div className="relative w-2/3 mt-20">
+          <input
+            className="w-full h-18  bg-white placeholder:font-italitc border border-slate-400 drop-shadow-md rounded-md py-2 pl-3 pr-10 outline-light text-dark"
+            placeholder="Search Coin"
+            type="text"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+        </div>
+      </div>
+      <CoinsHeadings />
+      <Coins searchTerm={searchTerm} />
     </div>
-    <div className="flex flex-col text-white pt-10">
-      <span className="font-lato font-extrabold text-2xl">$51.6B</span>
-      <span>Volume</span>
-    </div>
-    <div className="flex flex-col text-white pt-10 mr-4">
-      <span className="font-lato font-extrabold text-2xl">40%</span>
-      <span>BTC</span>
-    </div>
-  </div>
-);
+  );
+};
 
 export default CoinStats;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import CoinStats from './CoinStats';
-import CoinsHeadings from './CoinsHeadings';
-import Coins from './Coins';
 import NavBar from './NavBar';
 
 const Home = () => (
   <div>
     <NavBar />
     <CoinStats />
-    <CoinsHeadings />
-    <Coins />
   </div>
 );
 
